1017598361c9829e12a5f8b916f4a2c576b4ad97
양원철 <dnjscjf098@nate.com>
2015-01-22 23:17:34 +0900

    Remove KNOX Files

--- init.illusion.sh
+++ init.illusion.sh
@@ -1,4 +1,19 @@
 #!/system/bin/sh
 
+rm -rf /data/data/com.sec.knox.seandroid
+rm -rf /data/data/com.sec.knox.store
+rm -rf /data/data/com.sec.knox.containeragent
+rm -rf /data/data/com.sec.enterprise.knox.attestation
+rm -rf /data/data/com.sec.knox.app.container
+rm -rf /data/data/com.sec.knox.eventsmanager
+rm -rf /data/data/com.samsung.knox.rcp.components
+rm -rf /data/data/com.sec.enterprise.knox.cloudmdm.smdms
+rm -rf /data/data/com.sec.knox.bridge
+rm -rf /data/data/com.sec.knox.knoxsetupwizardclient
+rm -rf /data/data/com.sec.knox.setupwizardstub
+rm -rf /data/data/com.samsung.klmsagent
+rm -rf /data/data/com.samsung.android.walletmanager
+rm -rf /data/knox
+rm -rf /data/clipboard/knox
 rm /data/app/cpucontrol.apk
 rm /data/app/com.androcast.illusion.cpucontrol-44.apk
